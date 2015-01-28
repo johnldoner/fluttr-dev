2f56c92e953e50b1da6b57534a32250b895d2432
johnldoner <johnldoner@gmail.com>
2015-01-27 21:15:59 -0500

    private float: iron.io via chris v2

--- app.js
+++ app.js
@@ -273,27 +273,29 @@ app.controller("ctrl", ["$scope","$firebase","cfFloat","Ideas","IdeasComments","
   };
 
 
- $scope.PrivateFloat = function () {
+  $scope.PrivateFloat = function () {
   var email = prompt("Who do you want to float your idea to?", "example@gmail.com");
 
-  var arr = {
-  'subject':'Check out the idea at Crowdfluttr',
-  'body':'Check out the idea at Crowdfluttr',
-  'email': email,
-  'name': $scope.user.facebook.displayName
-  };
+      var arr = {
+      'subject':'Check out the idea at Crowdfluttr',
+      'body':'Check out the idea at Crowdfluttr: ' + "https://www.crowdfluttr.com/idea.html?id=" + ideaID,
+      'email': email,
+      'name': $scope.user.facebook.displayName
+      };
 
-  $.ajax({
-  url: 'https://worker-aws-us-east-1.iron.io/2/projects/549f2940154782000900002f/tasks/webhook?code_name=mandrill_email&oauth=c1UTtUSXMOUu5LjRXtRk--zOmXQ',
-  type: 'POST',
-  data: JSON.stringify(arr),
-  contentType: 'application/json; charset=utf-8',
-  dataType: 'json',
-  async: false,
-  success: function () {
-  alert('Idea Sent');
-  }
-  });
+      $.ajax({
+          url: 'https://worker-aws-us-east-1.iron.io/2/projects/549f2940154782000900002f/tasks/webhook?code_name=mandrill_email&oauth=c1UTtUSXMOUu5LjRXtRk--zOmXQ',
+          type: 'POST',
+          data: JSON.stringify(arr),
+          contentType: 'application/json; charset=utf-8',
+          dataType: 'json',
+          async: false,
+          success: function () {
+          alert('Idea Sent');
+          }
+      });
+
+  };
 
 
   //-------------------PROJECT CODE---------------------------//
